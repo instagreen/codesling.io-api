3d4eb6f15b334a1ee55dc79cc36eb5fa14f36ec8
Brian Duong <brndng89@gmail.com>
2018-03-06 11:44:24 -0800

    [buildEnv] created function to write env files

--- bin/buildEnv.js
+++ bin/buildEnv.js
@@ -1,13 +1,18 @@
-import { create } from 'domain';
-
 const fs = require('fs');
+const path = require('path');
 const envBuild = require('../config/.env.sample.js');
 
 // for each key, write file
 // ../rest-server/.env
 const createEnvFile = (envObj) => {
   for (let key in envObj) {
-    fs.writeFile(`../${key}/.env`, envBuild[key])
+    for (let i = 0; i < envObj[key].length; i++) {
+      fs.appendFile(path.join(__dirname, `/../${key}/.env`), `${envObj[key][i]}\n`, (err) => {
+        if (err){
+          console.log(err);
+        }
+      });
+    }
   }
 }
 
